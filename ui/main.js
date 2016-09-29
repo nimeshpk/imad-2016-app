c62f97714800f028f83bb48c1fdce833408655c4
nimeshpk <nimesh.p.k@gmail.com>
2016-09-30 01:39:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,10 @@ var element = document.getElementById('main-text');
 
 
 element.innerHTML = 'New Value';
+
+//Move the Image
+var img= document.getElementById('img');
+img.onclick = function () {
+  
+    img.style.marginLeft = '100px';
+};
