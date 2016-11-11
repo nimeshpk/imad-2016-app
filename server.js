5c266eca8f2b9859b62b3a69abe6ede0a65a1cc4
nimeshpk <nimesh.p.k@gmail.com>
2016-11-11 18:10:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').Pool;
+//var pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
 
@@ -80,13 +80,14 @@ app.post('/create-user', function (req, res) {
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+   //
+   //pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
      if (err) {
         res.status(500).send(err.toString());
     } else {
         res.send('User sucessfully created: ' + username);
     }   
-   });
+   //});
 });
 
 app.post('/login', function (req, res) {
