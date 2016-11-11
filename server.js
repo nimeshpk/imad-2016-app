c7e5e442dc9f443847981317ec9e6082b5e92342
nimeshpk <nimesh.p.k@gmail.com>
2016-11-11 18:18:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get('/submit-name', function (req, res) { // URL :/submit-name?name=xxxx
    res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName', function (req, res) {
+//app.get('/articles/:articleName', function (req, res) {
     // article name = article-one
     // articles[articleName] == {} content object for article one
     
