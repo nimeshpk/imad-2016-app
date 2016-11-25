10dbc1f04996fbbb605e07f65f495ac0859b2e14
nimeshpk <nimesh.p.k@gmail.com>
2016-11-26 03:18:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,18 +153,18 @@ app.get('/articles/:articleName', function (req, res) {
     // articles[articleName] == {} content object for article one
     
     // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-    pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function (err, result) {
+   pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function (err, result) {
      if (err) {
         res.status(500).send(err.toString());
-        } else {
+     } else {
             if (result.rows.length === 0) {
                 res.status(404).send('Article Not found');
-            } else {
-                var articleData = result.rows[0];
-                 res.send(createTemplate(articleData));
-            }
-     } 
-   }); 
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    } 
+  }); 
 });
 
 app.get('/ui/style.css', function (req, res) {
